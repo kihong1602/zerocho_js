75ce8c6f40b8250932ff75cea385d56d4fd595d4
BlanC <74227460+kihong1602@users.noreply.github.com>
2025-01-20 12:20:02 +0900

    📝 Create 3 week day 1

---
+++ week3/day1/example.js
@@ -0,0 +1,25 @@
+// 1. 2에서 5까지의 숫자를 뽑고 싶습니다. 제대로 작동하도록 (가) ~ (라)를 모두 채우세요.
+
+// (가)
+const answer1 = [];
+for (let n = 0; n < 4; n++) {
+  answer1.push(n + 2);
+}
+
+// (나)
+const answer2 = [];
+for (let n = 0; n <= 3; n++) {
+  answer2.push(n + 2);
+}
+
+// (다)
+const answer3 = [];
+for (let n = 1; n < 5; n++) {
+  answer3.push(n + 1);
+}
+
+// (라)
+const answer4 = [];
+for (let n = 1; n <= 4; n++) {
+  answer4.push(n + 1);
+}
